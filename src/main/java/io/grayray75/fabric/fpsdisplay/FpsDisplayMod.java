07e4afd773437f194b92286da5e35c7207ed6a8f
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-06-26 13:16:17 +0200

    Update to new fapi

--- src/main/java/io/grayray75/fabric/fpsdisplay/FpsDisplayMod.java
+++ src/main/java/io/grayray75/fabric/fpsdisplay/FpsDisplayMod.java
@@ -1,30 +1,28 @@
 package io.grayray75.fabric.fpsdisplay;
 
+import io.grayray75.fabric.fpsdisplay.config.FpsDisplayConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.legacyfabric.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.legacyfabric.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.options.KeyBinding;
-
 import org.apache.logging.log4j.LogManager;
 import org.lwjgl.input.Keyboard;
-
-import io.grayray75.fabric.fpsdisplay.config.FpsDisplayConfig;
 
 @Environment(EnvType.CLIENT)
 public class FpsDisplayMod implements ClientModInitializer {
 
-	@Override
-	public void onInitializeClient() {
-		LogManager.getLogger().info("Initializing FPS-Display Mod");
+    @Override
+    public void onInitializeClient() {
+        LogManager.getLogger().info("Initializing FPS-Display Mod");
 
-		KeyBinding binding_toggleOverlay = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle FPS overlay", Keyboard.KEY_NONE, "FPS-Display"));
+        KeyBinding binding_toggleOverlay = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle FPS overlay", Keyboard.KEY_NONE, "FPS-Display"));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (binding_toggleOverlay.wasPressed()) {
-				FpsDisplayConfig.enabled = !FpsDisplayConfig.enabled;
-			}
-		});
-	}
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (binding_toggleOverlay.wasPressed()) {
+                FpsDisplayConfig.enabled = !FpsDisplayConfig.enabled;
+            }
+        });
+    }
 }
